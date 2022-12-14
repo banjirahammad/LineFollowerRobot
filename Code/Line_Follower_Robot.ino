d429507affdca675487bd8c94c76b436634478d7
banjirahammad <banjirahammad@gmail.com>
2022-12-14 20:41:18 +0600

    Upload Line follower Robot

---
+++ Code/Line_Follower_Robot.ino
@@ -0,0 +1,70 @@
+/*
+ * Line Follower Robot
+ */
+ 
+ #define PWMmotor1 5 
+ #define Motor11 6 //LEFT MOTOR
+ #define Motor12 7
+ #define Motor21 8 //RIGHT
+ #define Motor22 9
+ #define PWMmotor2 10
+ 
+void setup() {
+  
+  pinMode(Motor11,OUTPUT);  //left motor
+  pinMode(Motor12,OUTPUT);  //left motor
+  pinMode(Motor21,OUTPUT);  //right motor
+  pinMode(Motor22,OUTPUT);  //right motor
+  pinMode(PWMmotor1,OUTPUT);   //left 
+  pinMode(PWMmotor2,OUTPUT);   //right
+
+  pinMode(12, INPUT); // initialize Right sensor as an inut
+  pinMode(11, INPUT); // initialize Left sensor as as input
+
+}
+
+void loop() {
+
+    int LEFT_SENSOR = digitalRead(11);
+    int RIGHT_SENSOR = digitalRead(12);
+
+    if(RIGHT_SENSOR==LOW && LEFT_SENSOR==LOW) //FORWARD
+    {
+            digitalWrite(Motor11, LOW);
+            digitalWrite(Motor12, HIGH);
+            digitalWrite(Motor21, LOW);
+            digitalWrite(Motor22, HIGH);
+            analogWrite(PWMmotor1, 250);
+            analogWrite(PWMmotor2, 250);
+    }
+
+    else if(RIGHT_SENSOR==HIGH && LEFT_SENSOR==LOW) //LEFT
+    {
+            digitalWrite(Motor11, LOW);
+            digitalWrite(Motor12, HIGH);
+            digitalWrite(Motor21, LOW);
+            digitalWrite(Motor22, HIGH);
+            analogWrite(PWMmotor1, 250);
+            analogWrite(PWMmotor2, 10);
+    }
+
+    else if(RIGHT_SENSOR==LOW && LEFT_SENSOR==HIGH) //RIGHT
+    {
+              digitalWrite(Motor11, LOW);
+              digitalWrite(Motor12, HIGH);
+              digitalWrite(Motor21, LOW);
+              digitalWrite(Motor22, HIGH);
+              analogWrite(PWMmotor1, 10);
+              analogWrite(PWMmotor2, 250);
+    }
+
+    else if(RIGHT_SENSOR==HIGH && LEFT_SENSOR==HIGH) //STOP
+    {
+              digitalWrite(Motor11, LOW);
+              digitalWrite(Motor12, LOW);
+              digitalWrite(Motor21, LOW);
+              digitalWrite(Motor22, LOW);
+    }
+
+    
+}
--- README.md
+++ README.md
@@ -1,1 +1,36 @@
-# LineFollowerRobot
+
+# Line Follower Robot
+
+The line follower robot is a mobile machine that can detect and follow the line drawn on the floor. Generally, the path is predefined and can be either visible like a black line on a white surface with a high contrasted color or it can be invisible like a magnetic field.
+
+# Components
+- Arduino UNO
+- IR sensor x 2
+- DC Gear Motor x 4
+- L298N Motor driver
+- Battery 7.4 x 2
+- Battery Holder
+- Motor wheel x 4
+- Wire m-f x 20
+- PVC board 20 x 12 cm
+
+## Software
+-  Arduino IDE
+
+
+## Programming Language
+-  Arduino Programming Language
+## Goals
+
+The project aims to create a line follower robot able to follow a path and reach its destination. Sensing a line and maneuvering the robot to stay on course while constantly correcting wrong moves using feedback from sensors forms a simple yet effective system. 
+## Features
+
+- Follow line
+- Taking various degrees of turns
+
+
+## 🔗 Links
+[![portfolio](https://img.shields.io/badge/my_portfolio-000?style=for-the-badge&logo=ko-fi&logoColor=white)](https://www.banjir-ahammad.com/)
+[![linkedin](https://img.shields.io/badge/linkedin-0A66C2?style=for-the-badge&logo=linkedin&logoColor=white)](https://www.linkedin.com/in/banjirahammad)
+[![twitter](https://img.shields.io/badge/twitter-1DA1F2?style=for-the-badge&logo=twitter&logoColor=white)](https://twitter.com/banjirahammad)
+
